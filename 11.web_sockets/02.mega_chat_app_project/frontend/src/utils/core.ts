1cc257c63ec28b096ad4647796bb0c2e801a504b
ahadsts9901 <ahadsts990@gmail.com>
2025-01-16 13:41:20 +0500

    code

--- 11.web_sockets/02.mega_chat_app_project/frontend/src/utils/core.ts
+++ 11.web_sockets/02.mega_chat_app_project/frontend/src/utils/core.ts
@@ -1,2 +1,2 @@
-export const baseUrl = ""
+export const baseUrl = "http://127.0.0.1:5000"
 export const defaultProfilePicture = "https://res.cloudinary.com/do6sd9nyx/image/upload/v1706343891/we-app-nextjs/Assets/profile-picture_ufgahm.png"
