c694abd6df23b6f1edb04b220814bb7dacc7f8c2
ahadsts9901 <ahadsts990@gmail.com>
2025-01-16 12:03:39 +0500

    schema

--- 11.web_sockets/02.mega_chat_app_project/app/models.py
+++ 11.web_sockets/02.mega_chat_app_project/app/models.py
@@ -4,7 +4,6 @@ from datetime import datetime
 class User(Document):
     username = StringField(required=True, unique=True)
     password = StringField(required=True)
-    role = StringField(default="user")  # user, admin, etc.
     profile_picture = StringField()
     created_at = DateTimeField(default=datetime.utcnow)
     updated_at = DateTimeField(default=datetime.utcnow)
@@ -13,8 +12,27 @@ class User(Document):
         return {
             "id": str(self.id),
             "username": self.username,
-            "role": self.role,
             "profile_picture": self.profile_picture,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
+
+
+
+class Chat(Document):
+    id = StringField(required=True)
+    from_id = StringField(required=True)
+    to_id = StringField(required=True)
+    text = StringField(required=True)
+    created_at = DateTimeField(default=datetime.utcnow)
+    updated_at = DateTimeField(default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            "id": str(self.id),
+            "from_id": str(self.from_id),
+            "to_id": str(self.to_id),
+            "text": str(self.text),
             "created_at": self.created_at,
             "updated_at": self.updated_at
         }
