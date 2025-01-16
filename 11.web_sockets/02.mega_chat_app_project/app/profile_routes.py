912dd8a9593dee63fb1b0ecfa660027c87dd4e97
ahadsts9901 <ahadsts990@gmail.com>
2025-01-16 12:22:35 +0500

    profile

--- 11.web_sockets/02.mega_chat_app_project/app/profile_routes.py
+++ 11.web_sockets/02.mega_chat_app_project/app/profile_routes.py
@@ -2,24 +2,52 @@ from flask import Blueprint, request, jsonify
 from .middleware import jwt_required
 from .models import User
 
-user_bp = Blueprint('user', __name__)
+profile_bp = Blueprint('user', __name__)
 
 # Get current user profile
-@user_bp.route('/profile', methods=['GET'])
+@profile_bp.route('/profile', methods=['GET'])
 @jwt_required
 def get_current_user_profile():
-    user_id = request.current_user['id']
-    user = User.objects(id=user_id).first()
-    if not user:
-        return jsonify({'message': 'user not found'}), 404
+    try:
+        user_id = request.current_user['id']
+        user = User.objects(id=user_id).first()
+        if not user:
+            return jsonify({'message': 'user not found'}), 404
 
-    return jsonify({
-        'message': 'profile fetched',
-        'data': {
-            'id': str(user.id),
-            'username': user.username,
-            'profile_picture': user.profile_picture,
-            'created_at': user.created_at,
-            'updated_at': user.updated_at
-        }
-    }), 200
+        return jsonify({
+            'message': 'current user profile fetched',
+            'data': {
+                'id': str(user.id),
+                'username': user.username,
+                'profile_picture': user.profile_picture,
+                'created_at': user.created_at,
+                'updated_at': user.updated_at
+            }
+        }), 200
+
+    except Exception as e:
+        return jsonify({"message": "internal server error", "error": str(e)}), 500
+
+
+# Get dynamic user profile
+@profile_bp.route('/profile/<string:user_id>', methods=['GET'])
+@jwt_required
+def get_current_user_profile(user_id):
+    try:
+        user = User.objects(id=user_id).first()
+        if not user:
+            return jsonify({'message': 'user not found'}), 404
+    
+        return jsonify({
+            'message': 'dynamic user profile fetched',
+            'data': {
+                'id': str(user.id),
+                'username': user.username,
+                'profile_picture': user.profile_picture,
+                'created_at': user.created_at,
+                'updated_at': user.updated_at
+            }
+        }), 200
+
+    except Exception as e:
+        return jsonify({"message": "internal server error", "error": str(e)}), 500
